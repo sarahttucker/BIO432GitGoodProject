abace31d7cdd772aea6e1046dcbdb27eb273448a
Liam Joiner <14lj10@queensu.ca>
2019-03-31 18:53:59 -0400

    Add files via upload

---
+++ Merging summary_metadata and distanced code.R
@@ -0,0 +1,18 @@
+Dist<-read.csv("MBTrial_tray-stem_distances.csv")
+Summary<-read.csv("summary_metadata.csv")
+Summary["distance"]<-NA
+head(Combined)
+
+  
+Summary$uid <- as.character(Summary$uid) 
+Dist$uid <- as.character(Dist$uid) 
+i1 <- which(names(Summary) == "distance") 
+
+Combined<-merge(Summary[-i1], Dist, by = "uid") 
+  
+  
+Combined$angle=NULL
+  
+write.csv(Combined, file = "summary_metadata_distance.csv")
+
+
