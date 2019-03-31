d2320b192f01264d1e3e355c8555ebc206d73e89
Pallavi Gutpa <14pg12@queensu.ca>
2019-03-31 19:15:15 -0400

    merging data frames to create summary table

---
+++ merging.R
@@ -0,0 +1,25 @@
+# merge 
+
+library(plyr)
+library(dplyr)
+
+reads <- read.csv("DropMap.csv")
+metsum <- read.csv("summary_metadata.csv")
+dist <- read.csv("MBTrial_tray-stem_distances.csv")
+
+metsum <- join(metsum, dist, by = "uid")
+head(metsum)
+
+metsum <- select(metsum, -c("combinationnotes", "combi", "project", "subproject", "well", "angle"))
+reads <- select(reads, -("X"))
+head(reads$sample)
+head(metsum)
+
+IDs <- gsub(".*-", "", metsum$seqname)
+editsample <- gsub("_.*", "", reads$sample)
+
+spliteditsample <- strsplit(IDs, split = "")
+
+for (i in spliteditsample) {
+  if ()
+}
